c1027c570676f4268bc3c44f4b38fd9fb9f9e793
Khoi Nguyen <k.nguyen5464@gmail.com>
2020-03-05 21:36:37 -0800

    update group proj

--- group proj/main-scene.js
+++ group proj/main-scene.js
@@ -1,87 +1,49 @@
-window.Assignment_Four_Scene = window.classes.Assignment_Four_Scene =
-class Assignment_Four_Scene extends Scene_Component
-  { constructor( context, control_box )     // The scene begins by requesting the camera, shapes, and materials it will need.
-      { super(   context, control_box );    // First, include a secondary Scene that provides movement controls:
-        if( !context.globals.has_controls   ) 
-          context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) ); 
+import {tiny, defs} from './examples/common.js';
+                                                  // Pull these names into this module's scope for convenience:
+const { Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, Light, Shape, Material, Shader, Texture, Scene,
+        Canvas_Widget, Code_Widget, Text_Widget } = tiny;
 
-        context.globals.graphics_state.camera_transform = Mat4.look_at( Vec.of( 0,0,5 ), Vec.of( 0,0,0 ), Vec.of( 0,1,0 ) );
+    // Now we have loaded everything in the files tiny-graphics.js, tiny-graphics-widgets.js, and common.js.
+    // This yielded "tiny", an object wrapping the stuff in the first two files, and "defs" for wrapping all the rest.
 
-        const r = context.width/context.height;
-        context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1000 );
+    // ******************** Extra step only for when executing on a local machine:  
+    //                      Load any more files in your directory and copy them into "defs."
+    //                      (On the web, a server should instead just pack all these as well 
+    //                      as common.js into one file for you, such as "dependencies.js")
 
-        // TODO:  Create two cubes, including one with the default texture coordinates (from 0 to 1), and one with the modified
-        //        texture coordinates as required for cube #2.  You can either do this by modifying the cube code or by modifying
-        //        a cube instance's texture_coords after it is already created.
-        const shapes = { box:   new Cube(),
-                         box_2: new Cube(),
-                         axis:  new Axis_Arrows()
-                       }
-        this.submit_shapes( context, shapes );
+const Minimal_Webgl_Demo = defs.Minimal_Webgl_Demo;
+import { Axes_Viewer, Axes_Viewer_Test_Scene } 
+  from "./examples/axes-viewer.js"
+import { Inertia_Demo, Collision_Demo }
+  from "./examples/collisions-demo.js"
+import { Many_Lights_Demo }
+  from "./examples/many-lights-demo.js"
+import { Obj_File_Demo }
+  from "./examples/obj-file-demo.js"
+import { Scene_To_Texture_Demo }
+  from "./examples/scene-to-texture-demo.js"
+import { Surfaces_Demo }
+  from "./examples/surfaces-demo.js"
+import { Text_Demo }
+  from "./examples/text-demo.js"
+import { Transforms_Sandbox }
+  from "./examples/transforms-sandbox.js"
 
-        // TODO:  Create the materials required to texture both cubes with the correct images and settings.
-        //        Make each Material from the correct shader.  Phong_Shader will work initially, but when 
-        //        you get to requirements 6 and 7 you will need different ones.
-        this.materials =
-          { phong: context.get_instance( Phong_Shader ).material( Color.of( 1,1,0,1 ) )
-          }
+Object.assign( defs,
+                     { Axes_Viewer, Axes_Viewer_Test_Scene },
+                     { Inertia_Demo, Collision_Demo },
+                     { Many_Lights_Demo },
+                     { Obj_File_Demo },
+                     { Scene_To_Texture_Demo },
+                     { Surfaces_Demo },
+                     { Text_Demo },
+                     { Transforms_Sandbox } );
 
-        this.lights = [ new Light( Vec.of( -5,5,5,1 ), Color.of( 0,1,1,1 ), 100000 ) ];
+    // ******************** End extra step
 
-        // TODO:  Create any variables that needs to be remembered from frame to frame, such as for incremental movements over time.
+// (Can define Main_Scene's class here)
 
-      }
-    make_control_panel()
-      { // TODO:  Implement requirement #5 using a key_triggered_button that responds to the 'c' key.
-        
-      }
-    display( graphics_state )
-      { graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
-        const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
+const Main_Scene = Transforms_Sandbox;
+const Additional_Scenes = [];
 
-        // TODO:  Draw the required boxes. Also update their stored matrices.
-        this.shapes.axis.draw( graphics_state, Mat4.identity(), this.materials.phong );
-      }
-  }
-
-class Texture_Scroll_X extends Phong_Shader
-{ fragment_glsl_code()           // ********* FRAGMENT SHADER ********* 
-    {
-      // TODO:  Modify the shader below (right now it's just the same fragment shader as Phong_Shader) for requirement #6.
-      return `
-        uniform sampler2D texture;
-        void main()
-        { if( GOURAUD || COLOR_NORMALS )    // Do smooth "Phong" shading unless options like "Gouraud mode" are wanted instead.
-          { gl_FragColor = VERTEX_COLOR;    // Otherwise, we already have final colors to smear (interpolate) across vertices.            
-            return;
-          }                                 // If we get this far, calculate Smooth "Phong" Shading as opposed to Gouraud Shading.
-                                            // Phong shading is not to be confused with the Phong Reflection Model.
-          vec4 tex_color = texture2D( texture, f_tex_coord );                         // Sample the texture image in the correct place.
-                                                                                      // Compute an initial (ambient) color:
-          if( USE_TEXTURE ) gl_FragColor = vec4( ( tex_color.xyz + shapeColor.xyz ) * ambient, shapeColor.w * tex_color.w ); 
-          else gl_FragColor = vec4( shapeColor.xyz * ambient, shapeColor.w );
-          gl_FragColor.xyz += phong_model_lights( N );                     // Compute the final color with contributions from lights.
-        }`;
-    }
-}
-
-class Texture_Rotate extends Phong_Shader
-{ fragment_glsl_code()           // ********* FRAGMENT SHADER ********* 
-    {
-      // TODO:  Modify the shader below (right now it's just the same fragment shader as Phong_Shader) for requirement #7.
-      return `
-        uniform sampler2D texture;
-        void main()
-        { if( GOURAUD || COLOR_NORMALS )    // Do smooth "Phong" shading unless options like "Gouraud mode" are wanted instead.
-          { gl_FragColor = VERTEX_COLOR;    // Otherwise, we already have final colors to smear (interpolate) across vertices.            
-            return;
-          }                                 // If we get this far, calculate Smooth "Phong" Shading as opposed to Gouraud Shading.
-                                            // Phong shading is not to be confused with the Phong Reflection Model.
-          vec4 tex_color = texture2D( texture, f_tex_coord );                         // Sample the texture image in the correct place.
-                                                                                      // Compute an initial (ambient) color:
-          if( USE_TEXTURE ) gl_FragColor = vec4( ( tex_color.xyz + shapeColor.xyz ) * ambient, shapeColor.w * tex_color.w ); 
-          else gl_FragColor = vec4( shapeColor.xyz * ambient, shapeColor.w );
-          gl_FragColor.xyz += phong_model_lights( N );                     // Compute the final color with contributions from lights.
-        }`;
-    }
-}
+export { Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs }
